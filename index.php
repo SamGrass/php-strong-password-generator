93f52901f96203ef5bc973a7a1c6275c48c6e4ca
Samgrass <samuelegrassetti01@gmail.com>
2024-07-25 19:19:15 +0200

    creati array da utilizzare per password

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 // creo array che contiene lettere maiuscole e minuscole
+$lower_alphabet = range("a","z");
+$upper_alphabet = range("A","Z");
+$alphabet = array_merge($lower_alphabet, $upper_alphabet);
 // creo array che contiene i numeri da 0 a 1
+$nums = range(0,9);
+var_dump($nums);
 // creo array che contiene dei caratteri speciali
-
+$specials = ['!','?','&','%','$','<','>','^','+','-','*','/','(',')','[',']','{','}','@','#','_','=',];
+var_dump($specials);
 
 ?>
 
