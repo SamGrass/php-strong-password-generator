ab4bd0e242357aae99eae11cc6eb9d4e35a737bb
Samgrass <samuelegrassetti01@gmail.com>
2024-07-25 21:10:40 +0200

    creata pagina esterna quando si crea la password

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . "/functions.php";
 
-
 // creo array che contiene lettere maiuscole e minuscole
 $lower_alphabet = range("a","z");
 $upper_alphabet = range("A","Z");
@@ -14,8 +13,15 @@ $specials = ['!','?','&','%','$','<','>','^','+','-','*','/','(',')','[',']','{'
 $password_characters = array_merge($alphabet, $nums, $specials);
 
 // utilizzo la funzione per tornarmi una password
-if (isset($_GET['length'])) {
+if (isset($_GET['length']) && !empty($_GET['length'])) {
     $password = randomPassword($_GET['length'], $password_characters);
+
+    // creo sessione per segnare la password
+    session_start();
+
+    $_SESSION['password'] = $password;
+
+    header('Location: ./password.php');
 } else {
     $password = '';
 }
@@ -27,27 +33,29 @@ if (isset($_GET['length'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container text-center">
-        <h1>Password generator</h1>
+        <h1 class="mt-3">Password generator</h1>
         <?php if (!isset($_GET['length'])): ?>
-            <div>Genera una password di lunghezza compresa tra 8 e 32</div>
+            <div class="ms_card">Genera una password di lunghezza compresa tra 8 e 32</div>
         <?php elseif (empty($_GET['length'])): ?>
-            <div>Errore: seleziona la lunghezza della password</div>
+            <div  class="ms_card">Errore: seleziona la lunghezza della password</div>
         <?php else: ?>
-            <div>La tua password è: <?php echo $password ?></div>
+            <div>La tua password è: <?php echo $_SESSION ?></div>
         <?php endif; ?>
 
         <!-- form con la selezione delle caratteristiche della password -->
-        <form action="index.php" method="GET">
-            <label for="length-password">Lunghezza password: </label>
-            <input type="number" name="length" id="length-password" min="8" max="32"> <br>
+        <div class="ms_card ms_card-form">
+            <form action="index.php" method="GET">
+                <label class="mb-4" for="length-password">Lunghezza password: </label>
+                <input type="number" name="length" id="length-password" min="8" max="32"> <br>
 
-            <button class="btn btn-primary" type="submit">Invia</button>
-        </form>
-
+                <button class="btn btn-primary" type="submit">Invia</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
