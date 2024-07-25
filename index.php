61b0caa0b1b59cab82047043855637e89bce1cc4
Samgrass <samuelegrassetti01@gmail.com>
2024-07-25 19:12:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// creo array che contiene lettere maiuscole e minuscole
+// creo array che contiene i numeri da 0 a 1
+// creo array che contiene dei caratteri speciali
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
